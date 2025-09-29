e6f4b7ede4f3442039ff824ac6077b4eddd2bbb8
Lieshan29 <161321584+Lieshan29@users.noreply.github.com>
2025-09-29 15:15:31 +0200

    Uploaded code

---
+++ src/main/java/com/mycompany/q1_prog6112/CricketRunsAnalyzer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.q1_prog6112;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Q1_PROG6112 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
